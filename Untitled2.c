5bc2f7f6857a4d75f717ac1220c30f595383e037
Sukhmeet Kaur <aeksukhi@gmail.com>
2018-03-17 14:17:49 +0530

    commit 3

--- Untitled2.c
+++ Untitled2.c
@@ -27,16 +27,36 @@ int process[4]={1,2,3,4};
 	int complete[4]={0};
 	while(1)
 	{
+		int k;
+		for(k=0;k<4;k++)
+		{
+			if(timer>=arrival[k] && burst[k]!=(-1)) 
+			{
+				priority[k]=1+((timer-arrival[k])/burst[k]);
+			}
+		}
 		int p,q;
 		for(p=0;p<4;p++)
 		{
 			for(q=0;q<4-1-p;q++)
 			{
-				if(priority[q]<priority[q+1] )
+				if(priority[q]<priority[q+1] && process_arrived[q]!=0 && process_arrived[q+1]!=0)
 				{
 					double t1=priority[q];
 					priority[q]=priority[q+1];
 					priority[q+1]=t1;
+					int t2=process_arrived[q];
+					process_arrived[q]=process_arrived[q+1];
+					process_arrived[q+1]=t2;
+				}
+				else if(priority[q]==priority[q+1]  && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					if(burst[q]<burst[q+1])
+					{
+						int t=process_arrived[q];
+						process_arrived[q]=process_arrived[q+1];
+						process_arrived[q+1]=t;
+					}
 				}
 			}
 		}
