7939d520950b1ba1df2d62b880ff2392d8e4cc8a
Sukhmeet Kaur <aeksukhi@gmail.com>
2018-03-31 14:24:22 +0530

    commit 7

--- Untitled1.c
+++ Untitled1.c
@@ -25,7 +25,6 @@ compute the scheduler performance by providing the waiting time for process, tur
 for process and average waiting time and turnaround time.
 */
 
-
 #include<stdio.h>
 #include<stdbool.h>
 struct Processes
@@ -33,6 +32,93 @@ struct Processes
 	int p_id;
 	int b_time,ar_time;
 };
+void waiting_time_of_process(struct Processes process[],int number_of_p,int waiting_time[])
+{
+	
+	int temp[number_of_p],value=0;
+	int time=0,least_burst_time_p=0,executed=0,f_time,min_time_of_execution=5000;
+	bool flag=false;
+	int x;
+	for(x=0;x<number_of_p;x++)
+{
+	temp[x]=process[x].b_time;
+}
+	for(x=0;x<number_of_p;x++)
+{
+	printf("%d\n",temp[x]);
+}
+	
+	while(executed!=number_of_p)
+	{
+		
+		for(x=0;x<number_of_p;x++)
+		{
+			
+			if((process[x].ar_time<=time)&&(temp[x]<min_time_of_execution&&temp[x]>0))
+			{
+				min_time_of_execution=temp[x];
+				least_burst_time_p=x;
+				flag=true;
+			}
+			
+		}
+		
+		if(flag==false)
+		{
+			time++;
+			continue;
+		}
+		
+		value=temp[least_burst_time_p];
+		time=time+value;
+		temp[least_burst_time_p]=0;
+		min_time_of_execution=5000;
+		if(temp[least_burst_time_p]==0)
+		{
+			executed++;
+			f_time=time+1;
+			waiting_time[least_burst_time_p]=f_time-process[least_burst_time_p].b_time-process[least_burst_time_p].ar_time+2;
+			
+			
+		}
+		
+		
+	}
+} 
+
+void turn_around_time_of_process(struct Processes process[],int number_of_p,int waiting_time[],int turn_around_time[])
+{
+	int a;
+	for(a=0;a<number_of_p;a++)
+	{
+		turn_around_time[a]=process[a].b_time+waiting_time[a];
+		 
+	}
+}
+
+
+
+
+
+void find_average_A_time(struct Processes process[],int number_of_p)
+{
+	int waiting_time[number_of_p],turn_around_time[number_of_p];
+	int total_waiting_time=0,total_turn_around_time=0;      //int
+	
+	waiting_time_of_process(process,number_of_p,waiting_time);
+	turn_around_time_of_process(process,number_of_p,waiting_time,turn_around_time);
+	
+	printf("| Processes |\t| Burst time |\t| Waiting time |\t| Turn around time | \n");
+	int x;
+	for(x=0;x<number_of_p;x++)
+	{
+		total_waiting_time=total_waiting_time+waiting_time[x];
+		total_turn_around_time=total_turn_around_time+turn_around_time[x];
+		printf("  %d \t\t%d \t\t%d \t\t%d\n",process[x].p_id,process[x].b_time,waiting_time[x],turn_around_time[x]);
+	}
+		printf("\nAverage waiting time of processes : %f",(float)total_waiting_time / (float)number_of_p);
+    	printf("\nAverage turn around time of processes : %f",(float)total_turn_around_time / (float)number_of_p);
+}
 int main()
 {
 	int no_of_processes;
@@ -47,6 +133,16 @@ int main()
 		scanf("%d",&process[i].p_id);
 		printf("\nEnter the arrival time : ");
 		scanf("%d",process[i].ar_time);
+		while(process[i].ar_time==0)
+		{
+			printf("\nEntered process arrival time is wrong. Enter the correct arrival time \nArrival Time : ");
+			scanf("%d",process[i].ar_time);
+		}
+		printf("\nEnter the burst time : ");
+		scanf("%d",process[i].b_time);	
+		i++;
 	}
+	find_average_A_time(process,no_of_processes);
+	return 0;	
 }
 
